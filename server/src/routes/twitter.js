d12eef2b4246c56019e192871098692fddd900fe
sjsujomoon <jeongook.moon@sjsu.edu>
2018-12-03 23:57:07 -0800

    will work on getting my tweets and post

--- server/src/routes/twitter.js
+++ server/src/routes/twitter.js
@@ -44,18 +44,5 @@ router.get('/latest-tweets', function (req, res) {
     res.status(200).send(tweets);
   });
 });
-
-// Gets the ten most recent tweets that use the hashtag 'payroll'
-router.get('/latest-tweets', function (req, res) {
-  const params = {
-    q: '#payroll',
-    count: '10',
-    result_type: 'recent'
-  };
-  twitterClient.get('/search/tweets', params, function(error, tweets, response) {
-    res.status(200).send(tweets);
-  });
-});
-
 module.exports = router;
 
